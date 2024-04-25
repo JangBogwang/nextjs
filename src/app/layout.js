6de8c32451bcd3bc3f31766c9ff6de5f1f5ac893
jangbogwang <wkdqhrhkd12@gmail.com>
2024-04-25 12:13:14 +0900

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <h1>hello</h1>
+      <h2>hello</h2>
     </html>
   );
 }
